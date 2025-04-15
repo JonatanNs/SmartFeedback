23b5c456985b25564d6720252c9caf092807151d
JonatanNs <nsualu.jonatan@gmail.com>
2025-04-15 02:41:21 +0200

    Add project skeleton: initial backend, frontend folder, Docker setup, and PostgreSQL configuration

---
+++ backend/src/main/java/com/SmartFeedback/SmartFeedback/SmartFeedbackApplication.java
@@ -0,0 +1,13 @@
+package com.SmartFeedback.SmartFeedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFeedbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartFeedbackApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/SmartFeedback/SmartFeedback/SmartFeedbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SmartFeedback.SmartFeedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFeedbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
